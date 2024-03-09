44f4c0b8b4173a9027594ba9b60399e6a51d6c87
Lucas S <lucas.sorribes@gmail.com>
2024-03-09 15:24:07 +0100

    chore: FRONTEND: add initial imports to App.jsx

--- demo-app/frontend/src/App.jsx
+++ demo-app/frontend/src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import loginService from './services/login'
+import quoteService from './services/quoteService'
+import LoginForm from './components/LoginForm'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null)
+  const [notification, setNotification] = useState(null)
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBloglistUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
 
   return (
     <>
