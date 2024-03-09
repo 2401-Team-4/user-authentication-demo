8b39850017b59d9a1ab5d5cd78235e4c28a34c04
Lucas S <lucas.sorribes@gmail.com>
2024-03-09 15:37:28 +0100

    build: FRONTEND: add basic React components

---
+++ demo-app/frontend/src/components/Quote.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+import QuoteDetails from "./QuoteDetails"
+
+const Quote = ({ quote, likeQuote, deleteQuote, user }) => {
+  const [visible, setVisible] = useState(false)
+
+  const quotestyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+    marginRight: 20
+  }
+
+  return (
+    <div className='quote' style={quotestyle} id={quote.title}>
+      <h5 style={{ display: 'inline-block' }}>{`${quote.title}`} by {`${quote.author}`} </h5>
+      <button onClick={() => setVisible(!visible)}>{visible ? 'hide' : 'view'}</button>
+      <QuoteDetails
+        quote={quote}
+        visible={visible}
+        likeQuote={likeQuote}
+        deleteQuote={deleteQuote}
+        user={user}
+      ></QuoteDetails>
+    </div>
+  )
+}
+
+export default Quote
